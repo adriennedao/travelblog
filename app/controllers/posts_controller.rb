ebf7a2d8f511b249862bf1034db82326528231ce
AdrienneD <adriennedao@gmail.com>
2016-12-21 23:27:15 -0500

    Added method/controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @post = Post.all
+    @posts = Post.all
 end
 
   def show
@@ -12,13 +12,26 @@ end
   @post = Post.new
 end
 
-  def edit
- end
-
   def create
      Post.create!(post_params)
      redirect_to posts_path
-end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
 private
   def set_post
